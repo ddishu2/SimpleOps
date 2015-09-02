74bc9c364dee4712b756702ebe12179a3a8e4b02
d-ptellis_802931 <d-ptellis_802931@c5804cb6-0653-4972-b0c7-f5ec3eedd9b6>
2015-09-02 15:52:21 +0000

    ptellis 02/09: Updated namingConventions

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,18 +1,20 @@
 <?php
-
 class cl_Routes
 {
     const GC_ROUTE_OPEN_SO          = '/open_so(/)'; 
     const GC_ROUTE_DEPLOYABLE_EMPS  = '/deployable_emp(/)'; 
-    const GC_ROUTE_EMP_FOR_SO       = '/emps_for_open_so(/)'; 
+    const GC_ROUTE_AUTO_PROPOSE     = '/auto_propose(/)'; 
+    const GC_ROUTE_SOFT_LOCK        = '/soft_lock(/)';
+    const GC_ROUTE_HARD_LOCK        = '/hard_lock(/)';
+            
 }
 
 //Include Slim Framework Library Code
 class cl_RMGTool_Globals
 {
     const GC_APP_NAME               = 'RMGTool_REST_API';
-    const GC_ROUTE_OPEN_SO          = '/open_so(/)'; 
-    const GC_ROUTE_DEPLOYABLE_EMPS  = '/deployable_emp(/)'; 
+    const GC_ROUTE_OPEN_SO          = '/open_sos(/)'; 
+    const GC_ROUTE_DEPLOYABLE_EMPS  = '/deployable_emps(/)'; 
     const GC_ROUTE_EMP_FOR_SO       = '/emps_for_open_so(/)'; 
     const OPEN_SO_DATE_RANGE    = 21;
 
@@ -20,13 +22,9 @@ class cl_RMGTool_Globals
 
 
 //Include Slim.php library files
-require __DIR__ .
-        DIRECTORY_SEPARATOR .
-        'libraries' .
-        DIRECTORY_SEPARATOR .
-        'Slim' .
-        DIRECTORY_SEPARATOR .
-        'Slim.php';
+require __DIR__ .DIRECTORY_SEPARATOR .'libraries' .DIRECTORY_SEPARATOR .'Slim' 
+        .DIRECTORY_SEPARATOR .'Slim.php';
+
 require __DIR__.DIRECTORY_SEPARATOR.'cl_DB.php';
 require __DIR__.DIRECTORY_SEPARATOR.'cl_vo_deployableEmp.php';
 require __DIR__.DIRECTORY_SEPARATOR.'cl_vo_open_sos.php';
@@ -35,10 +33,11 @@ require __DIR__.DIRECTORY_SEPARATOR.'cl_vo_open_sos.php';
  
 // Instantiate a Slim Application
  $app = new \Slim\Slim();
+
 // Set Name of App to identify the app while acquiring references
  $app ->setName(cl_RMGTool_Globals::GC_APP_NAME);
  
-// Define a HTTP GET Route
+// Define HTTP Routes
  
 // GET route for Open SOs
  $app->get
@@ -46,7 +45,6 @@ require __DIR__.DIRECTORY_SEPARATOR.'cl_vo_open_sos.php';
             cl_RMGTool_Globals ::GC_ROUTE_OPEN_SO, 
             function () use ($app)
             {
-                $re_it_emps_for_sos = [];
                 $lo_open_sos = new cl_vo_open_sos();
                 $lv_so_from_date = $app->request->get(cl_vo_open_sos::C_FNAME_SO_FROM);
                 $lv_so_to_date   = $app->request->get(cl_vo_open_sos::C_FNAME_SO_TO);            
@@ -57,7 +55,7 @@ require __DIR__.DIRECTORY_SEPARATOR.'cl_vo_open_sos.php';
         
 
 //  GET route for Auto Emp. Proposals                     
- $app->get('/auto_propose(/)', 
+ $app->get(cl_Routes::GC_ROUTE_AUTO_PROPOSE, 
                function () use($app) 
                {
      
