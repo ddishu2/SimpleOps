040c639a572970f5bf0842b9536f2d7a55bb11d2
d-ptellis_802931 <d-ptellis_802931@c5804cb6-0653-4972-b0c7-f5ec3eedd9b6>
2015-09-03 14:45:26 +0000

    ptellis 03/09: Improved performance of Cl_DataFilter

--- api/v1/cl_Lock.php
+++ api/v1/cl_Lock.php
@@ -17,10 +17,22 @@ public function __construct(){}
 public function getSoftLockedEmps(){}
 public function getHardLockedEmps(){}
 public function getSoftLockExpiredEmps(){}
-public function setSoftLock($fp_v_emp_corp_id, $fp_v_so_id){}
-public function setHardLock($fp_v_emp_corp_id, $fp_v_so_id){}
-public function rejectProposal($fp_v_emp_corp_id, $fp_v_so_id){}
-public function rejectSoftLock($fp_v_emp_corp_id, $fp_v_so_id){}
+public function setSoftLock($fp_v_lock_trans_id)
+{
+//    
+}
+public function setHardLock($fp_v_lock_trans_id)
+{
+//    
+}
+public function rejectProposal($fp_v_proposal_id)
+{
+    
+}
+public function rejectSoftLock($fp_v_lock_trans_id)
+{
+    
+}
 
 
         
