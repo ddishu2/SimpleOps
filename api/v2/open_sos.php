4f1d597592cd6e8f13e045c98c786b74fa666af4
d-ptellis_802931 <d-ptellis_802931@c5804cb6-0653-4972-b0c7-f5ec3eedd9b6>
2015-10-27 12:57:09 +0000

    Added a query builder to add common filters

---
+++ api/v2/open_sos.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Summary:  Contains Logic to Process Open SOs(query).
+ * 
+ * 
+ * @author "Prashanth Tellis Prashanth.Tellis@capgemini.com"
+ * Date: 16/09/2015.
+ * @uses cl_DB()->getResultsFromQuery to retrieve results. 
+ * 
+ */
+
+require_once __DIR__.DIRECTORY_SEPARATOR.'cl_DB.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'open_so_query_builder.php';
+class open_sos extends open_so_query_builder
+{
+
+    private $arr_open_sos = []; 
+    
+    
+    const C_DATE_FORMAT   = 'Y-m-d';
+
+    
+    const C_DATE_COMPONENTS = 3;
+    protected $v_so_sdate;
+    protected $v_so_endate;
+   
+    public static $arr_lockedso = [];
+    
+    function __construct($fp_v_so_sdate , $fp_v_so_endate)
+    {
+        parent::__construct($fp_v_so_sdate , $fp_v_so_endate);
+    }
+        
+    public function get( ) 
+    {
+        $this->setOpenSOs();  
+        return $this->arr_open_sos;
+    }
+    
+    
+    private function setOpenSOs()
+    {
+        $larr_open_sos = [];
+        $larr_sos =  $this->fetch();
+        foreach ($larr_sos as $lwa_so) 
+        {
+            $lv_so_id = $lwa_so[self::C_SO_ID_DB_FNAME];
+            $larr_open_sos[] = $lwa_so;
+        }
+        $this->arr_open_sos =  $larr_open_sos;
+    }
+
+    private function fetch()
+    {
+        $re_sos   = [];
+        $lv_query = parent::getQuery();
+//        echo $lv_query;
+        $re_sos = cl_DB::getResultsFromQuery($lv_query);
+        return $re_sos;
+    }
+    
+    public function isOpen($fp_v_so_id) 
+    {
+//      //  print_r(self::$arr_lockedso);
+//          $re_open = false;
+////        $v_so_rejectionCountWithinLimits = $this->isSO_RejectionCountWithinLimits($fp_v_so_id);
+////        $v_so_unfulfilled                = $this->isSO_Unfulfilled($fp_v_so_id);
+////        $lv_isOpen                       = $v_so_unfulfilled && $v_so_rejectionCountWithinLimits;
+//        $lv_query = 'SELECT'.PHP_EOL
+//                    .'so_no'.PHP_EOL
+//                    .'FROM'.PHP_EOL
+//                    .'v_open_so'.PHP_EOL
+//                    .'LIMIT 1';
+//        $lv_so_id = cl_DB::getResultsFromQuery($lv_query);
+//        $lv_count = cl_DB::getCountAndReset();
+//        if($lv_count === 1)
+//        {
+            $re_open = true;
+//        }
+        return $re_open;
+    }
+    
+    private function isSO_RejectionCountWithinLimits()
+    {
+//     
+    }
+    
+/**
+* Returns true if no emps. currently soft/hard locked against so
+*@return true|false
+ *  */
+    private function isSO_Unfulfilled()
+    {
+        
+    }
+ // changes by tejas
+    
+  private function getLocked()
+  {
+      $sql = "select so_id from trans_locks where status in ('S121','S201')";
+      self::$arr_lockedso = cl_DB::getResultsFromQuery($sql);
+//             print_r(self::$arr_lockedso);
+  }
+    
+   
+    
+    private function isSOLocked($fp_v_so_id)
+    {
+      // print_r(self::$arr_lockedso);
+        $lv_slocked = false;
+        foreach (self::$arr_lockedso as $key => $value) 
+        {
+            if(in_array($fp_v_so_id,$value))
+            {
+                $lv_slocked = true;
+                break;
+            }
+        }
+        $re_slocked = $lv_slocked;
+        return $re_slocked;
+    }
+    
+}
