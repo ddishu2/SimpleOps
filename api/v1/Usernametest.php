827578bd34245033f65bde4130317432522a2e3a
dikmishr <dikmishr@c5804cb6-0653-4972-b0c7-f5ec3eedd9b6>
2015-10-28 15:43:34 +0000

    Updated

---
+++ api/v1/Usernametest.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Here you get the Auth_User wich comes out to Domain\User
+//print_r($_SERVER['PHP_AUTH_USER']);
+echo 'CooG';
+var_dump($_GET);
+echo 'CooP';
+var_dump($_POST);
+echo 'CooR';
+var_dump($_REQUEST);
+echo 'Coo';
+var_dump($_COOKIE);
